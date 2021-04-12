eebf9faff9a2a48501e374fda5e7cd95e28ff8b7
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2021-04-12 16:02:13 -0300

    Styled components/dark mode

---
+++ src/DarkMode.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import styled, {css} from 'styled-components'
+
+//Coloque o elemento que você estiver fazendo, por exemplo, se for um P, coloque style.p
+//Sempre coloque o elemento fora do componente.
+const Button = styled.button`
+    //Estilos condicionais "inline"
+    //background: ${props => props.dark ? "#EEEEEE" : "#333333"};
+    
+    //Estilos condicionais "inblock"
+    ${
+        props => props.dark ? css`
+            background: #EEEEEE;
+            color: #333333;
+            border-color: #333333;
+        `
+        :
+        css`  
+            background: #333333;
+            color: #EEEEEE;
+            border-color: #EEEEEE;
+        `
+    }
+
+    border-radius: 10px;
+    padding: 5px;
+    outline: none;
+`;
+
+const Container = styled.div`
+    background: ${props => props.dark ? "#333333" : "#EEEEEE"};
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+function DarkMode() {
+    const [dark, setDark] = useState(false);
+
+    return (
+        <Container dark={dark}>
+            <Button dark={dark} onClick={() => setDark(!dark)}>Modo {dark ? "claro" : "escuro"}</Button>
+        </Container>
+    )
+}
+
+export default DarkMode;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import App from './App';
 import Login from "./Login";
+import DarkMode from "./DarkMode";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -11,6 +12,7 @@ ReactDOM.render(
     <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Login} />
+        <Route path="/dark-mode" component={DarkMode} />
         <Route path="/postagens/page=:page" component={App} />
       </Switch>
     </Router>
